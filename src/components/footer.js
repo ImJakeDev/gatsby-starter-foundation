36fc9a56c280c0ee4d602d811eeef17b2503f503
Jake Schaffer <jake.schaffer@pxdbproject.com>
2020-10-31 17:50:20 -0500

    added more custom colors to styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>A GatsbyJS Starter for Netlify CMS, Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Stackrole.com</Link></p>
+      <p>A GatsbyJS website with a Netlify CMS, Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Jake</Link></p>
     </div>
   </footer>
 )
